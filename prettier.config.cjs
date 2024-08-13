7f137139f82bc6352d64740474382acc77aee057
Truong Minh Phat <tmphat1312@gmail.com>
2024-08-13 16:45:55 +0700

    refactor: move prettier config to workspace scope

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  endOfLine: 'lf',
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-packagejson'],
+};
+
+module.exports = config;
