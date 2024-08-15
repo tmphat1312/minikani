80d105f14d91b54d4cda401cc1f35477bbf426da
Truong Minh Phat <tmphat1312@gmail.com>
2024-08-15 19:35:44 +0700

    refactor: change config format

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -4,15 +4,15 @@ import globals from 'globals';
 import ts from 'typescript-eslint';
 
 export default ts.config(
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  prettier,
+  { ignores: ['**/*.config.*'] },
   {
     files: ['**/*.{ts,tsx,js,jsx}'],
+    extends: [js.configs.recommended, ...ts.configs.recommended, prettier],
     rules: {
       'no-unused-vars': 'warn',
       'no-undef': 'warn',
       'no-console': 'warn',
+      'prefer-const': 'off',
     },
     languageOptions: {
       globals: {
